c954f75abeeeb0c0a9461782bea9d6082c207d00
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-26 21:58:20 +0200

    feat: Navbar

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './utils/style/index.css'
+import Navbar from './Components/Navbar';
 import Home from './pages/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Navbar/>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
   </React.StrictMode>
 )
