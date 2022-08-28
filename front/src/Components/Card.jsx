5136e7e2762bdd8fa6d9d7f8ce851fe78eb2c664
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-08-28 11:04:37 +0200

    feat: PropTypes + JSdoc

---
+++ front/src/Components/Card.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+import Img from "./Img"
+import PropTypes from 'prop-types'
+
+/**
+ * Component to create a card
+ * @param {string} imgSrc - path of the svg icon.
+ * @param {string} bgColor - background-color of the card.
+ * @param {string} label - label of the icon.
+ * @param {string} width - width of the icon.
+ * @param {string} height - height of the icon.
+ * @returns JSX for a card with an icon.
+ */
+export default function LittleCard({imgSrc, bgColor, label, width, height}) {
+  return (
+	<>
+		<StyledCard $bgColor={bgColor} $width={width} $height={height}>
+			<Img src={imgSrc} alt={label} />
+		</StyledCard>		
+	</>
+  )
+}
+
+const StyledCard = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #FF0101;
+	border-radius: 6px;
+	width: ${({$width}) => $width};
+	height: ${({$height}) => $height};
+	background-color: ${({$bgColor}) => $bgColor};
+`
+
+LittleCard.propTypes = {
+	imgSrc : PropTypes.string,
+	bgColor : PropTypes.string,
+	label : PropTypes.string,
+	width : PropTypes.string,
+	height : PropTypes.string,
+}
