9b494831b97d36cd8abfa071a89fdb2defe156f7
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-09-02 23:04:44 +0200

    feat HomePage

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components"
+import StyledLink from "../Components/StyledLink"
+
 export default function Home() {
   return (
-	<h1>Home</h1>
+	<Wrapper>
+		<H1>Bienvenue sur <Span color="#FF0101">SportSee</Span></H1>
+		<P marginTop="1rem">Projet 12 de la formation OpenClassrooms, création d'un tableau de bord avec React et D3.js. Cliquez sur l'identifiant d'un utilisateur pour voir ses données :</P>
+		<P marginTop="1rem">
+			<StyledLink to="/user/12">User 12</StyledLink>
+		</P>
+		<P>
+			<StyledLink to="/user/18">User 18</StyledLink>
+		</P>
+	</Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+	margin-left: 130px;
+	margin-top: 20px;
+`
+const H1 = styled.h1`
+	font-size: 40px;
+`
+const Span = styled.span`
+	color: ${({color}) => color};
+`
+const P = styled.p`
+	margin-top: ${({marginTop}) => marginTop};
+`
