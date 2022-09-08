b706a61c90390b53501506a651eb4e7ad1a4ae47
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-09-08 22:19:30 +0200

    feat: tooltips

--- front/src/Components/D3/BarChart.jsx
+++ front/src/Components/D3/BarChart.jsx
@@ -8,16 +8,14 @@ export default function BarChart({data, svgHeight}) {
 	//svg parent ref
 	const chartContainerRef = useRef()
 	//ref for resize event
-	const update = useRef(false)
-	// tooltips ref
-	const tooltipRef = useRef(false)
+	const updateBars = useRef(false)
 	//responsive width
 	const { viewportWidth } = useViewport()
 
 	useEffect(() => {
 		
 		//if resize remove the previous chart
-		update.current ? d3.selectAll('svg').remove() : update.current = true
+		updateBars.current ? d3.select('.barChart-svg').remove() : updateBars.current = true
 		// re-draw the chart with new dimensions after resize 
 		DrawChart(data)
 
@@ -28,12 +26,14 @@ export default function BarChart({data, svgHeight}) {
 	
 	const DrawChart = (data) => {
 
+		//dimentions
 		const graphWidth = parseInt(d3.select(chartContainerRef.current).style('width')) - margin.left - margin.right
 		const graphHeight = parseInt(d3.select(chartContainerRef.current).style('height')) - margin.top - margin.bottom
-		
+				
 		// create new chart
 		const svg = d3.select(chartContainerRef.current)
 			.append('svg')
+			.classed('barChart-svg', true)
 			.attr('width', graphWidth + margin.left + margin.right)
 			.attr('height', graphHeight + margin.top + margin.bottom)
 			.style('background-color', '#F5F7F9')
@@ -49,16 +49,16 @@ export default function BarChart({data, svgHeight}) {
 		const extent = d3.extent(data.map(d => new Date(d.day).getDate()))
 		const x_Scale = d3.scaleLinear()
 			.domain(extent)
-			.range([margin.left, graphWidth])
+			.range([margin.left, graphWidth - margin.right])
 
 		const x_Axis = d3.axisBottom(x_Scale)
 			.tickSize(0)
-			.tickPadding([margin.bottom])
+			.tickPadding(margin.bottom)
 			.ticks(7)
 
 		svg.append('g')
 			.call(x_Axis)
-			.attr('transform', `translate(${0}, ${graphHeight + margin.bottom})`)
+			.attr('transform', `translate(0, ${graphHeight + margin.bottom})`)
 			.attr("font-size", "1rem")
 			.select('path')
 			.attr('transform', 'scale(1.03) translate(-11,0)')
@@ -78,7 +78,7 @@ export default function BarChart({data, svgHeight}) {
 		const y_Axis = d3.axisRight(y_Weight_Scale)
 			.ticks(3)
 			.tickSize(0)
-			.tickPadding([10])
+			.tickPadding(20)
 			
 		svg.append('g')
 			.call(y_Axis)
@@ -89,13 +89,13 @@ export default function BarChart({data, svgHeight}) {
 		//grille
 		const gridTickValues = y_Axis.scale().ticks(3).slice(1)
 		const yAxisGrid = d3.axisLeft(y_Weight_Scale)
-			.tickSize(graphWidth - margin.right - 10)
+			.tickSize(graphWidth - margin.right -margin.left)
 			.tickFormat('')
 			.tickValues(gridTickValues)
 		svg.append('g')
 			.style("stroke-dasharray", ("3, 3"))
 			.style('color', 'lightgray')
-			.attr('transform', `translate(${graphWidth + margin.right - 10}, 0)`)
+			.attr('transform', `translate(${graphWidth - margin.right}, 0)`)
 			.call(yAxisGrid)
 			.select('path').remove()
 
@@ -194,10 +194,70 @@ export default function BarChart({data, svgHeight}) {
 			.attr( 'stroke', "#E60000")
 			.attr('stroke-width', "8")
 			.attr('stroke-linecap',"butt")
+		// tooltips
+		data.forEach((d, index) => { 
+			let group = svg.append("g")
+				.attr("id", `day${index+1}`)
+			// create gray rectangles for hover
+			group.append("rect")
+				.attr("x", x_Scale(index+1) - 40)
+				.attr("y", margin.top + 4)
+				.attr("width", 80)
+				.attr("height", 195)
+				.attr('fill', 'gray')
+				.attr("opacity", "0")
+				// make it appear on hover + make the infos appears
+				.on("mouseover", function () {
+					d3.select(this).transition()
+						.duration("150")
+						.attr("opacity", ".2")
+					d3.selectAll(`#day${index+1} > *:not(:first-child)`).transition()
+						.duration("150")
+						.attr("opacity", "1")
+					})
+				.on("mouseout", function () {
+					d3.select(this).transition()
+						.duration("150")
+						.attr("opacity", "0")
+					d3.selectAll(`#day${index+1} > *:not(:first-child)`).transition()
+						.attr("opacity", "0")
+					})
+			// infos bubble 
+			group.append("rect")
+				.attr("x", displayTooltip(index+1) + 25)
+				.attr("y", 40)
+				.attr("width", 60)
+				.attr("height", 70)
+				.attr("opacity", "0")
+				.attr('fill', 'red')
+			group.append("text")
+				.attr("x", displayTooltip(index+1) + 55)
+				.attr("y", 65)
+				.text(d.kilogram + "Kg")  
+				.style("text-anchor", "middle")
+				.style("font-size", "1vw")
+				.style('fill', '#fff')
+				.attr("opacity", "0")
+			group.append("text")
+				.attr("x", displayTooltip(index+1) + 55)
+				.attr("y", 95)
+				.text(d.calories + "Kcal")   
+				.style("text-anchor", "middle")
+				.style("font-size", "1vw")
+				.style('fill', '#fff')   
+				.attr("opacity", "0")
+		})
+		// Just to be sure a tooltip don't go outside the chart
+		function displayTooltip(index) {
+			if(x_Scale(index) <= graphWidth -margin.left -margin.right) 
+				return x_Scale(index)
+			else 
+				return x_Scale(index) -100
+		}
+
 	}
 
 	return <div className="barchart" ref={chartContainerRef} style={{width:(viewportWidth/2 + margin.left + margin.right), height:svgHeight}}>
-		<div ref={tooltipRef} className="tooltip"><span id="tooltip-label"></span></div>
 	</div>
 
 }
