bd0575a2a22ce337050c0595112c00a97481d9b9
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-09-09 21:18:18 +0200

    feat: clean code

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
-import { ViewportProvider } from './utils/Context/ViewportContext'
-import './utils/style/index.css'
-import Navbar from './Components/Navbar';
-import Home from './pages/Home'
-import Stats from './pages/Stats';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import { ViewportProvider } from "./utils/Context/ViewportContext"
+import "./utils/style/index.css"
+import Navbar from "./Components/Navbar"
+import Home from "./pages/Home"
+import Stats from "./pages/Stats"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+	<React.StrictMode>
 		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<ViewportProvider>
-				<Navbar/>
+				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/user/:id" element={<Stats />} />
 				</Routes>
 			</ViewportProvider>
 		</BrowserRouter>
-  </React.StrictMode>
+	</React.StrictMode>
 )
